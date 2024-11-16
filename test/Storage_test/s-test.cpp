f8afd71f9596beabc1e110212c803ab39f2da4da
Sheriaties <sherlock617@163.com>
2024-11-16 14:09:30 +0800

    删除了在不属于store中的sqlite库 创造了一个数据存储的测试程序 删除了之间的测试程序

---
+++ test/Storage_test/s-test.cpp
@@ -0,0 +1,12 @@
+#include <storage/sqlite3.h>
+
+#include <iostream>
+#include <ostream>
+
+#include "storage/storage.hpp"
+using namespace  std;
+int main() {
+  Storage storage;
+  cout << "Hello World!" << endl;
+
+}
