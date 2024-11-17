c46a3c6b0ae28d569f070bfe2d8a1d13ceba0004
Sheriaties <sherlock617@163.com>
2024-11-17 21:58:39 +0800

    删除了在不属于store中的sqlite库 创造了一个数据存储的测试程序 删除了之间的测试程序 在add_user函数中的参数改成了const string &的形式 在构造函数中添加了打开数据库和打开表的功能 实现了add_user函数 添加了s-test作为一个数据存储的测试程序 写了一个判断密码是否正确的函数，但是如果name 相同有bug

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -14,7 +14,7 @@ Storage::Storage() {
   }
   //先打开数据库
   const char* create_tableSQL = R"(CREATE TABLE IF NOT EXISTS Users_list (ID INTEGER PRIMARY KEY,
-                                    Name TEXT NOT NULL, Password TEXT NOT NULL);)";
+                                    Name TEXT NOT NULL, Password TEXT NOT NULL, Tag TEXT NOT NULL DEFAULT '0000000');)";
   if(sqlite3_exec(db, create_tableSQL, nullptr, nullptr, nullptr) != SQLITE_OK) {
     std::cerr << "Error creating table" << std::endl;
     sqlite3_close(db);
@@ -23,8 +23,6 @@ Storage::Storage() {
     std::cout << "Table created successfully" << std::endl;
   }
 }
-
-
 Storage::~Storage() {
   std::cout << "Storage destructor" << std::endl;
   sqlite3_close(db);
@@ -49,6 +47,44 @@ bool Storage::addUser(const std::string& username, const std::string& password) 
     return false;
   }
   sqlite3_finalize(stmt);
+  std::cout<<"The pass word is :"<< password << std::endl;
   std::cout<<"Successfully added"<<std::endl;
   return true;
+}
+
+bool Storage::authenticateUser(const std::string& username, const std::string& password) {
+  if(!db) {
+    std::cout<<"Error opening database"<<std::endl;
+    return false;
+  }
+  const char* querySQL = "SELECT Password FROM Users_list WHERE Name=?; ";
+  sqlite3_stmt *stmt;
+  if(sqlite3_prepare_v2(db, querySQL, -1, &stmt, nullptr) != SQLITE_OK) {
+    std::cout<<"Error preparing statement"<<std::endl;
+    return false;
+  }
+  sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_STATIC);
+  std::string cor_password;
+  if(sqlite3_step(stmt) != SQLITE_ROW) {
+    std::cout<<"Error executing statement"<<std::endl;
+    return false;
+  }
+  if(sqlite3_step(stmt) == SQLITE_ROW) {
+    const char* pw = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+    if(pw) {
+      cor_password = pw;
+    }
+    else {
+      std::cout<<"Error getting the user's name"<<std::endl;
+      return false;
+    }
+    if(cor_password == password) {
+      return true;
+    }
+    if(cor_password != password) {
+      std::cout<<"Wrong password"<<std::endl;
+      std::cout << "THe correct password is:" << cor_password << std::endl;
+      return false;
+    }
+  }
 }
